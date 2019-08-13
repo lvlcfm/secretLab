3bc715e9e6f8c16e4b7079707f774511c7340a22
Christopher Franco Monterrosa <himahmago@gmail.com>
2019-08-13 01:21:39 -0700

    clientssss

--- client/src/components/Lesson/CreateLesson.js
+++ client/src/components/Lesson/CreateLesson.js
@@ -14,12 +14,39 @@ class CreateSite extends Component {
       media: '',
       week: '',
       siteTime: '',
-      exitTicket: ''
+      exitTicket: '',
+      siteObj: '',
+      site_id: '',
+      day: '',
+      startTime: '',
+      endTime: '',
+      siteNumber: ''
     };
 
     this.change = this.change.bind(this);
+    this.handleSiteTimeChange = this.handleSiteTimeChange.bind(this);
 
     this.submit = this.submit.bind(this);
+  }
+
+  componentDidMount() {
+    axios
+      .get(`http://localhost:5000/api/sites/${this.props.match.params.id}`)
+      .then(res => {
+        // storing token from server
+        this.setState({
+          siteObj: res.data,
+          site_id: res.data._id,
+          day: res.data.siteTimes[0].day,
+          startTime: res.data.siteTimes[0].startTime,
+          endTime: res.data.siteTimes[0].endTime,
+          siteNumber: res.data.siteTimes[0].siteNumber,
+          siteTimes: res.data.siteTimes
+        });
+      })
+      .catch(err => {
+        console.log(err);
+      });
   }
 
   // takes an event and creates a key,value pair
@@ -28,12 +55,40 @@ class CreateSite extends Component {
       [event.target.name]: event.target.value
     });
   }
-
+  handleSiteTimeChange(event) {
+    console.log(event.target.value);
+    const newTimeList = this.state.siteTimes.filter(
+      site => parseInt(site.siteNumber) === parseInt(event.target.value)
+    );
+    const newTime = newTimeList[0];
+    console.log(newTime);
+    console.log('???');
+    this.setState({
+      day: newTime.day,
+      startTime: newTime.startTime,
+      endTime: newTime.endTime,
+      siteNumber: newTime.siteNumber
+    });
+  }
   submit(event) {
     event.preventDefault();
+    console.log('LETS SEND THIS');
+    console.log(this.state.day);
+    console.log(this.state.startTime);
+    console.log(this.state.endTime);
+    console.log(this.state.siteNumber);
     axios
       .post('http://localhost:5000/api/lessons', {
-        title: this.state.title
+        title: this.state.title,
+        site_id: this.state.site_id,
+        siteTime: [
+          {
+            day: this.state.day,
+            startTime: this.state.startTime,
+            endTime: this.state.endTime,
+            siteNumber: this.state.siteNumber
+          }
+        ]
       })
       .then(res => {
         // storing token from server
@@ -45,11 +100,9 @@ class CreateSite extends Component {
   }
 
   render() {
-    console.log(this.props);
     return (
       <div className="container">
         <div className="signUpBox">
-          <img src="../public/img/logo-lower.png" className="logo" />
           <div className="title">
             <div className="anova">CREATE A</div>
             <div className="labs">LESSON</div>
@@ -65,6 +118,25 @@ class CreateSite extends Component {
                   onChange={this.change}
                   value={this.state.title}
                 />
+              </label>
+            </div>
+            <div>
+              current day selected
+              <label>
+                Day
+                <select
+                  id="siteTime"
+                  name="siteTime"
+                  onChange={this.handleSiteTimeChange}
+                >
+                  {this.state.siteObj
+                    ? this.state.siteObj.siteTimes.map(siteTime => (
+                        <option key={siteTime._id} value={siteTime.siteNumber}>
+                          {`${siteTime.siteNumber} ${siteTime.startTime} ${siteTime.day}`}
+                        </option>
+                      ))
+                    : ''}
+                </select>
               </label>
             </div>
             <input type="submit" value="submit" />
--- client/src/components/Lesson/Lesson.js
+++ client/src/components/Lesson/Lesson.js
@@ -1,10 +1,7 @@
 import React, { Component } from 'react';
-import { Link, withRouter } from 'react-router-dom';
+import { withRouter } from 'react-router-dom';
 
 class Lesson extends Component {
-  constructor(props) {
-    super(props);
-  }
   render() {
     console.log(this.props.match.params.id);
     return <div>LESSON PLAN BABY</div>;
